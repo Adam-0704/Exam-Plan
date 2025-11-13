07271f058b756677c40982ba61532d2795a801b0
Adam-0704 <adamchino8@gmail.com>
2025-11-13 20:11:19 +0100

    jeg har lavet alle repositories som vi skal bruge til selve programmet.

---
+++ ClassLibrary/Repository/ExamRepo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary.model;
+
+namespace ClassLibrary.Repository
+{
+    internal class ExamRepo
+    {
+        private readonly DataAccess.ExamContext _context;
+
+        public ExamRepo(DataAccess.ExamContext context)
+        {
+            _context = context;
+        }
+
+        public List<Exam> GetAll() => _context.Exams.ToList();
+
+        public void Add(Exam exam)
+        {
+            _context.Exams.Add(exam);
+            _context.SaveChanges();
+        }
+
+        public void Update(Exam exam)
+        {
+            _context.Exams.Update(exam);
+            _context.SaveChanges();
+        }
+
+        public void Delete(Exam exam)
+        {
+            _context.Exams.Remove(exam);
+            _context.SaveChanges();
+        }
+    }
+}
